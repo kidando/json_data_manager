021701430ce57727ca8a78111f59355c3f179310
Leon Oscar Kidando <oscarlwoga@gmail.com>
2020-11-16 10:24:30 +0300

    updated ui to be even more native to vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
 <template>
   <v-app>
-    <v-row no-gutters>
-      <v-col>
- <div class="d-flex">
-        <Sidebar></Sidebar>
-        <router-view></router-view>
-      </div>
-      </v-col>
-     
-    
-    </v-row>
+    <v-navigation-drawer fixed v-model="drawer" app>
+      <!--  -->
+    </v-navigation-drawer>
+    <v-app-bar color="blue" dense flat  app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+<!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Sidebar from "./components/App/Sidebar.vue";
 
 export default Vue.extend({
   name: "App",
 
-  components: {
-    Sidebar,
-  },
+  components: {},
 
   data: () => ({
-    //
+    drawer: true,
   }),
 });
 </script>
